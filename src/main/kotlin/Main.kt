39894475903f2cd467aed3c18c748e896399dce4
Vincent Cubit <vinniecubit@yahoo.com>
2021-09-15 01:46:15 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    val myByte: Byte = 100
+    val myShort: Short = 4000
+    val myInt: Int = 70000
+    val myLong: Long = 14000000000
+    val myFloat: Float = 2.99F
+    val myDouble: Double = 29.99
+    val myBoolean: Boolean = true
+    val myChar: Char = 'A'
+
+
+    println("Byte: $myByte")
+    println("Short: $myShort")
+    println("Int: $myInt")
+    println("Long: $myLong")
+    println("Float: $myFloat")
+    println("Double: $myDouble")
+    println("Boolean: $myBoolean")
+    println("Char: $myChar")
+
+}
